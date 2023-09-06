07fae724079154d3f62fe18b1bf3c929daf62b3b
Nicholas Choong <nicholascwc1997@gmail.com>
2023-09-06 11:21:39 +0800

    wrap with main tag

--- web_app/src/pages/_app.tsx
+++ web_app/src/pages/_app.tsx
@@ -3,7 +3,11 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
