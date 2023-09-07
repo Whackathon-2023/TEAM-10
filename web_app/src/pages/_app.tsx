595e94cedefccf583a8c12d60d014ec354d7334f
Nicholas Choong <nicholascwc1997@gmail.com>
2023-09-07 11:57:16 +0800

    navbar

--- web_app/src/pages/_app.tsx
+++ web_app/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
+import NavBar from "~/components/navbar";
 
 import "~/styles/globals.css";
 
@@ -11,9 +12,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <meta name="description" content="A WHACK project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <div className="flex flex-row">
+        <NavBar />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
     </>
   );
 };
