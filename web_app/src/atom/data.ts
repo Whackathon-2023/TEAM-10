f8f614f26bc12e398851c7f6e31731f5073a93a8
Nicholas Choong <nicholascwc1997@gmail.com>
2023-09-08 15:06:18 +0800

    init jotai

---
+++ web_app/src/atom/data.ts
@@ -0,0 +1,15 @@
+import { atom } from "jotai";
+import { z } from "zod";
+
+const TankDataSchema = z.object({
+  tankName: z.string(),
+  tankSerialNumber: z.string(),
+  telemetryDatetimeEpoch: z.number(),
+  tankLevel: z.number(),
+  tankCustomerName: z.string(),
+});
+
+// Define the type for your TankData
+type TankData = z.infer<typeof TankDataSchema>;
+
+export const panelAtom = atom<TankData[]>([]);
