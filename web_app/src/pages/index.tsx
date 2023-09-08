0c1e291a590086d13241a82306761c89ad193b4c
Nicholas Choong <nicholascwc1997@gmail.com>
2023-09-08 11:55:40 +0800

    index

--- web_app/src/pages/index.tsx
+++ web_app/src/pages/index.tsx
@@ -4,16 +4,6 @@ import { useFieldArray, useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "~/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
-import {
   Card,
   CardContent,
   CardDescription,
@@ -23,84 +13,72 @@ import {
 } from "~/components/ui/card";
 import { useEffect, useRef } from "react";
 import TypographyH1 from "~/components/typography/h1";
-
-const tankSchema = z.object({
-  name: z.string().nonempty({ message: "Tank name is required" }),
-  serialNumber: z.coerce.number().positive({
-    message: "Serial number must be a positive number",
-  }),
-  dateTime: z
-    .string()
-    .regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/)
-    .refine(
-      (value) => {
-        const parsedDate = new Date(value);
-        return !isNaN(parsedDate.getTime());
-      },
-      {
-        message: "Invalid date and time format",
-      },
-    ),
-  delivery: z.coerce
-    .number()
-    .positive({ message: "Delivery must be a positive number" })
-    .int({ message: "Delivery must be an integer" })
-    .min(1, { message: "Delivery must be at least 1" })
-    .max(100000, { message: "Delivery cannot exceed 100,000" }),
-  customerName: z.string().nonempty({ message: "Customer name is required" }),
-});
-
-const tanksListSchema = z.array(tankSchema);
-
-const tanksSchema = z.object({
-  tank: tankSchema,
-  tanks: tanksListSchema.optional(),
-});
+import Link from "next/link";
 
 const Home = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof tanksSchema>>({
-    resolver: zodResolver(tanksSchema),
-  });
-  const { fields, append, remove } = useFieldArray({
-    control: form.control,
-    name: "tanks",
-  });
-
-  const lastItemRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (lastItemRef.current) {
-      lastItemRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [fields]);
-
-  // 2. Define a submit handler.
-  const onSubmit = ({ tank, tanks }: z.infer<typeof tanksSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    if (tanks && tanks.length > 0 && Array.isArray(tanks)) {
-      const tank_list = tanksListSchema.parse([tank, ...tanks]);
-      console.log(tank_list);
-    } else {
-      const tank_list = tanksListSchema.parse([tank]);
-      console.log(tank_list);
-    }
-  };
-
-  const animations = {
-    layout: true,
-    initial: { scale: 1, opacity: 0 },
-    animate: { scale: 1, opacity: 1 },
-    exit: { scale: 1, opacity: 0 },
-    transition: { type: "spring", stiffness: 900, damping: 40 },
-  };
-
   return (
-    <div className="flex min-h-screen flex-col items-center lg:ml-32 xl:ml-44 2xl:ml-72">
+    <div className="mx-10 flex min-h-screen flex-col items-center">
       <TypographyH1 className="mt-20 max-w-lg text-center text-6xl font-semibold text-grey-dark">
         Welcome to the Test Data Generator
       </TypographyH1>
       <hr className="mt-10 w-1/2 rounded border-t-4 border-black" />
+      <h2 className="mt-10 max-w-2xl text-center text-2xl font-semibold text-gray-400">
+        This website allows you to find out the necessary information regarding
+        the tank levels in different scenarios.
+      </h2>
+      <h3 className="my-28 max-w-2xl rounded-2xl bg-primary px-8 py-4 text-center text-xl text-white">
+        Please select a scenario to begin
+      </h3>
+      <div className="flex max-w-5xl flex-row gap-10">
+        <Link
+          href="/delivery"
+          className="transform transition duration-500 ease-in-out hover:scale-110"
+        >
+          <Card className="h-full transform transition duration-500 ease-in-out hover:shadow-md">
+            <CardHeader>
+              <CardTitle>Delivery</CardTitle>
+            </CardHeader>
+            <CardContent className="max-w-lg">
+              <p>
+                This scenario specifically updates the tank reading after the
+                delivery process to 3 customers.
+              </p>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link
+          href="/delivery-and-usage"
+          className="transform transition duration-500 ease-in-out hover:scale-110"
+        >
+          <Card className="h-full transform transition duration-500 ease-in-out hover:shadow-md">
+            <CardHeader>
+              <CardTitle>Delivery & Usage</CardTitle>
+            </CardHeader>
+            <CardContent className="max-w-lg">
+              <p>
+                This scenario specifically updates the tank reading after the
+                delivery process and its usage for one customer.
+              </p>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link
+          href="/leak"
+          className="transform transition duration-500 ease-in-out hover:scale-110"
+        >
+          <Card className="h-full transform transition duration-500 ease-in-out hover:shadow-md">
+            <CardHeader>
+              <CardTitle>Leakage</CardTitle>
+            </CardHeader>
+            <CardContent className="max-w-lg">
+              <p>
+                This scenario specifically updates the reading after a leakage
+                in one of the customers’ tank.
+              </p>
+            </CardContent>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 };
